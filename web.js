212cd599a886dd263ed5861fb6eb6fe03b24164e
plb696 <bhandarypranith69@gmail.com>
2023-06-20 16:22:32 +0530

    basic

---
+++ web.js
@@ -0,0 +1,15 @@
+// console.log('hello');
+// var a = 138
+// var b = 2
+// var c = a/b
+// console.log('the division of 138 is ',c);
+
+
+// let person = prompt("Please enter your name");
+
+// if (person != null) {
+//   document.getElementById("demo").innerHTML =
+//   "Hello " + person + "! How are you today?";
+// }
+
+
